301801d541fec568025db28b301979602a729063
ruthjomo <ruthjomo19@gmail.com>
2020-09-17 17:46:51 +0300

    changed codes

--- manage.py
+++ manage.py
@@ -1,6 +1,6 @@
 from app import create_app
 from flask_script import  Manager, Server
-import os
+
 
 
 app = create_app('development')
@@ -9,8 +9,8 @@ manager = Manager(app)
 manager.add_command('server',Server)
 
 
-# if __name__ == '__main__':
-#     manager.run()
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+if __name__ == '__main__':
+    manager.run()
+# if __name__ == "__main__":
+#     port = int(os.environ.get("PORT", 5000))
+#     app.run(host="0.0.0.0", port=port)
